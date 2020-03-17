21d785661cc3d4e683b4c5e3ecfc7b3d4b91aa89
wellingtonvargasgonzalez <milpetalosdeloto@gmail.com>
2020-03-17 11:29:07 +0100

    Version inicial del Planetario.

---
+++ app/src/main/java/com/example/planetario2020/Planeta01Fragment.java
@@ -0,0 +1,28 @@
+package com.example.planetario2020;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Planeta01Fragment extends Fragment {
+
+    public Planeta01Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_planeta01, container, false);
+    }
+}
